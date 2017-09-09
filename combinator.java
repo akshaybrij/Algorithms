bfda0b688345f0e1adf8ac3270bb73addc9e8c61
Akshay_Brijpuria_ <akshay.brijpuria@gmail.com>
2017-09-09 14:27:32 -0400

    Update combinator.java

--- combinator.java
+++ combinator.java
@@ -28,8 +28,43 @@ class Combinator{
     }
     return C[n][k];
   }
+  public int LongestSub(char[] X,char[] Y){
+    int m=X.length;
+    int n=Y.length;
+    int L[][]=new int[m+1][n+1];
+    for(int i=0;i<=m;i++){
+      for(int j=0;j<=n;j++){
+        if(i==0 || j==0){
+          L[i][j]=0;
+        }
+        else if(X[i-1]==Y[j-1]){
+          L[i][j]=L[i-1][j-1]+1;
+        }
+        else{
+          L[i][j]=max(L[i-1][j],L[i][j-1]);
+        }
+      }
+    }
+    return L[m][n];
+  }
+  public int max(int c,int b){
+    if(c>b){
+      return c;
+    }
+    else if(c==b){
+      return c;
+    }
+    else{
+      return b;
+    }
+  }
   public static void main(String...args){
     Combinator c=new Combinator();
     System.out.println(c.combine(4,2));
+    String X="AGGTAB";
+    String Y="GXTXAYB";
+    char[] arr=X.toCharArray();
+    char[] ary=Y.toCharArray();
+    System.out.println(c.LongestSub(arr,ary));
   }
 }
